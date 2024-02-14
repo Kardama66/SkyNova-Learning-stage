31d67d6523220b8aa68c5c321fdc7f101c6b1659
kardama66 <sebastianwiencek243@gmail.com>
2024-02-14 10:04:20 +0100

    creating form and validation

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config) #adding configuraion file
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,36 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
-    return render_template('index.html', title='Ziomo', user=user)#returning html template from file index.html
+    user = {'username': 'Radziu'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Steve'},
+            'body': 'Testing Everything'
+        },
+        {
+            'author': {'username': 'Rajesh'},
+            'body': 'I would like to eat apple'
+        }
+    ]
+    return render_template('index.html', title='Ziomo', user=user, posts=posts)#returning html template from file index.html
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm() #Taking class from other file (imported above)
+    if form.validate_on_submit(): #Submiting
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index') #Redirecting to index
+    return render_template('login.html', title='Sign In', form=form)
